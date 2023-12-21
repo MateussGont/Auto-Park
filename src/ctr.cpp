75ca8bdc515b4d2ed0a5b17a0a15c03c11b02346
MateussGont <75398575+MateussGont@users.noreply.github.com>
2023-12-20 22:38:26 -0300

    Merge commit

--- src/ctr.cpp
+++ src/ctr.cpp
@@ -4,58 +4,6 @@
 
 #define STOP -1
 
-class Ctr
-{
-public:
-    // Função para calcular a distância que o carro deve percorrer para entrar na vaga
-    double calculateDistanceToTravel(Car car, ParkingSpot spot)
-    {
-        // Calcular a distância entre o carro e a vaga de estacionamento
-        double distance = sqrt(pow(spot.length - car.length, 2) + pow(spot.width - car.width, 2));
-
-        return distance;
-    }
-
-    int stopMoving (Car car){
-        int state;
-        if ((car.sensor.readRangeContinuousMillimeters() == 15.0) || (car.sensor2.readRangeContinuousMillimeters() == 15.0))
-        {
-            //lógica para freio emergencial do carrinho
-            return state = STOP;
-        }
-    }
-
-    void lockVelocity(Car car){
-        //car.velocity deve ser setada para um valor fixo para facilitar calculos
-        //A velocidade depende de qual mecanismo será utilizado para o carro se movimentar
-
-
-    }
-
-    // Função para calcular o ângulo que o carro deve girar para entrar na vaga
-    double calculateAngleToTurn(Car car, ParkingSpot spot)
-    {
-        // Implementar a lógica para calcular o ângulo
-        return 0.0;
-    }
-
-    // Função para controlar o volante do carro
-    void controlVolante(double angle)
-    {
-        // Implementar a lógica para controlar volante
-    }
-
-    // Função para controlar a velocidade do carro
-    void controlSpeed(double distance)
-    {
-        // Implementar lógica para controlar velocidade
-    }
-
-    void center(Car car){
-        
-    }
-};
-
 /*int main()
 {
     Ctr ctr;
--- src/main.cpp
+++ src/main.cpp
@@ -2,215 +2,181 @@
 #include <Wire.h>
 #include "VL53L0X.h"
 #include "models.h"
-#include "stdlib.h"
-
-// definições de direção
-#define LEFT 0
-#define RIGHT 1
-#define FOWARD 2
-#define BACK 3
-
-// Definições de alerta
-#define OK 0
-#define DANGER -1
-#define SAFE 1
+#include "ctr.h"
 
 // Pipeline para entrar na vaga 180°
-#define PROCURANDO -1 // // Botão start apertado e inicio dos sensores
-#define PRONTO 0      // Painel exibe que encontrou uma vaga compativel
-#define CALIBRANDO 1  // alinhamento das traseiras dos carros (Carro encontra momento q a variação da distancia seja repentina)
-#define TURN 2        // Faz a rotação das rodas e anda para trás até atingir um angulo X (Medição a ser feita)
-#define ENTRY 3       // Após estar no angulo alinha as rodas ao centro e anda uma distancia X (Medição a ser feita)
-#define RETURN 4      // após andar a distancia anterior vira as rodas completamente a esquerda e continua a entrada até o sensor de ré apitar
-#define ENDUP 5       // volante volta ao centro e usuário termina com ajustes finos
-#define FINISHED 6    // Para os sensores
+#define CALIBRANDO 0 // alinhamento das traseiras dos carros
+#define INIT 1       // botão start apertado e inicio dos sensores
+#define TURN 2       // Faz a rotação das rodas e anda para trás até atingir um angulo X (Medição a ser feita)
+#define ENTRY 3      // Após estar no angulo alinha as rodas ao centro e anda uma distancia X (Medição a ser feita)
+#define RETURN 4     // após andar a distancia anterior vira as rodas completamente a esquerda e continua a entrada até o sensor de ré apitar
+#define ENDUP 5      // volante volta ao centro e usuário termina com ajustes finos
+#define FINISHED 6   // Para os sensores
 
-int state;
-Car carro = createCar(160, 70, 68); // carro em mm
+int state = INIT;
+String sidePark = "";
+String reverseSide = "";
+bool initPark = false;
+Car carro = createCar(5.1, 2.3, 2.1);
+Ctr States;
 // ParkingSpot spot = createParkingSpot();
 TwoWire I2Cone = TwoWire(0);
-VL53L0X sensor;
-VL53L0X sensor2;
+VL53L0X sensor_direita_frente;
+VL53L0X sensor_direita_tras;
+VL53L0X sensor_traseiro;
+VL53L0X sensor_esquerda_frente;
+VL53L0X sensor_esquerda_tras;
 uint16_t valor;
-uint16_t buffer1, bufffer2, buffer3;
 float scale[2] = {35.0, 22.0}; // passa a largura e comprimento do carro para medição de escala -verificar implementação
-int tipo_vaga = 1;             // tipo da vaga, 1 = 90°, 2 = baliza, 3 = 45°
-int direção;
-/*
-Futuramente será feito um método que freia o carro sozinho
 
-Envia um sinal para que o motor pare
-*/
 void freia_sozinho()
 {
-}
-
-int estaciona_90_graus(VL53L0X sensores)
-{
-  int done = 0;
-  while (done != 1)
-  {
-    switch ()
-    {
-    case /* constant-expression */:
-      /* code */
-      break;
-
-    default:
-      break;
-    }
-  }
-}
-
-/*
-  Verifica a unidade de medida do sensor
-
-  Posteriormente irá setar tambvem levar a setar
-  o valor de stage do carro em relação a vaga
-
-*/
-int verfica_sensor(VL53L0X sensor)
-{
-  if (sensor.timeoutOccurred())
-  {
-    Serial.print("TIMEOUT");
-  }
-  uint16_t dist = sensor.readRangeContinuousMillimeters();
-
-  if (dist < 10)
-  {
-    Serial.print("Cuidado");
-    return DANGER;
-  }
-  else if (dist >= 10 || dist < 30)
-  {
-    Serial.print("Distancia Ótima");
-    return OK;
-  }
-  else if (dist > 30)
-  {
-    Serial.print("Tranquilo");
-    return SAFE;
-  }
-}
-float read_vaga()
-{
-}
-int verifica_vaga(int tipo)
-{
-  // Implementar Outros tipos de vaga
-  if (tipo == 1)
-  { // Vaga 90°
-    while (sensor.readRangeContinuousMillimeters() < 5000)
-    {
-      Serial.println("Continue a ré");
-    }
-    if (sensor.readRangeContinuousMillimeters() >= 5000)
-    {
-      state++;
-      return 1;
-    }
-    else
-    {
-      return -1;
-    }
-  }
 }
 
 void setup()
 {
   Serial.begin(921600);
+  pinMode(15, OUTPUT);
   pinMode(16, OUTPUT);
   pinMode(17, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(19, OUTPUT);
+  delay(100);
+  digitalWrite(15, LOW);
   delay(100);
   digitalWrite(16, LOW);
   delay(100);
   digitalWrite(17, LOW);
   delay(100);
+  digitalWrite(18, LOW);
+  delay(100);
+  digitalWrite(19, LOW);
+  delay(100);
+  digitalWrite(15, HIGH);
+  delay(100);
   digitalWrite(16, HIGH);
+  delay(100);
+  digitalWrite(17, HIGH);
+  delay(100);
+  digitalWrite(18, HIGH);
+  delay(100);
+  digitalWrite(19, HIGH);
   delay(100);
   Wire.begin();
   // I2Cone.begin(21, 22, 921600); // Configuração de pinos para I2C gpio 21 e 22
   // sensor.setBus(&I2Cone);       // Leitor do sensor configurado para o barramento de entrada I2C
 
-  sensor.setTimeout(50);
-  if (!sensor.init())
+  sensor_esquerda_frente.setTimeout(50);
+  sensor_direita_frente.setTimeout(50);
+  sensor_esquerda_tras.setTimeout(50);
+  sensor_direita_tras.setTimeout(50);
+  sensor_traseiro.setTimeout(50);
+  if (!sensor_esquerda_frente.init() || !sensor_direita_frente.init() || !sensor_direita_tras.init() || !sensor_esquerda_tras.init() || !sensor_traseiro.init())
   {
     Serial.println("Failed to detect and initialize sensor 1!");
     while (1)
     {
     }
   }
-  sensor.setAddress((uint8_t)22);
-  delay(100);
-  digitalWrite(17, HIGH);
-
-  if (!sensor2.init())
-  {
-    Serial.println("Failed to detect and initialize sensor 2!");
-    while (1)
-    {
-    }
-  }
-
-  sensor2.setAddress((uint8_t)25);
+  sensor_esquerda_frente.setAddress((uint8_t)22);
+  sensor_direita_frente.setAddress((uint8_t)22);
+  sensor_esquerda_tras.setAddress((uint8_t)22);
+  sensor_direita_tras.setAddress((uint8_t)22);
+  sensor_traseiro.setAddress((uint8_t)22);
   // Start continuous back-to-back mode (take readings as
   // fast as possible).  To use continuous timed mode
   // instead, provide a desired inter-measurement period in
   // ms (e.g. sensor.startContinuous(100)).
-  sensor.startContinuous(100);
-  sensor2.startContinuous(100);
+  sensor_esquerda_frente.startContinuous(100);
+  sensor_esquerda_tras.startContinuous(100);
+  sensor_direita_tras.startContinuous(100);
+  sensor_direita_frente.startContinuous(100);
+  sensor_traseiro.startContinuous(100);
 }
 
 void loop()
 {
-  uint16_t dist1 = sensor.readRangeContinuousMillimeters();
-  if (sensor.timeoutOccurred())
-  {
-    Serial.print(" TIMEOUT");
-  }
-
-  uint16_t dist2 = sensor2.readRangeContinuousMillimeters();
-  if (sensor2.timeoutOccurred())
+  uint16_t distEF = sensor_esquerda_frente.readRangeContinuousMillimeters();
+  uint16_t distET = sensor_esquerda_tras.readRangeContinuousMillimeters();
+  uint16_t distDT = sensor_direita_tras.readRangeContinuousMillimeters();
+  uint16_t distDF = sensor_direita_frente.readRangeContinuousMillimeters();
+  uint16_t distT = sensor_traseiro.readRangeContinuousMillimeters();
+  if (sensor_esquerda_frente.timeoutOccurred() || sensor_esquerda_tras.timeoutOccurred() || sensor_direita_frente.timeoutOccurred() || sensor_direita_tras.timeoutOccurred() || sensor_traseiro.timeoutOccurred())
   {
     Serial.print(" TIMEOUT");
   }
 
   // States
-  switch (state)
+  if (initPark)
   {
-  case PRONTO:
-    // se sinal de pronto tiver sido enviado muda estado para calibrando
-    break;
-  case CALIBRANDO:
-    // Encontra o primeira variação de distancia grande
-    verifica_vaga(tipo_vaga);
-    break;
-  case TURN:
-    // Inicia a rotação das rotas até que o carro atinja angulo necessário (A ser testado) para proximo estágio
-    gira_rodas();
-    break;
-  case ENTRY:
-    // retorna as rodas e inicia o movimento de entrada na vaga até percorrer uma certa distancia
-    break;
-  case RETURN:
-    // faz a rotação das rodas para o lado oposto e continua entrada
-    break;
-  case ENDUP:
-    // termina de ajustar o carro  na vaga caso seja necessário
-    break;
-  case FINISHED:
-    // Termina a vaga e os sensores são desativados
-    break;
-  default:
-    break;
+    switch (state)
+    {
+    case CALIBRANDO:
+      // após passar uma vaga de um certo tamanho passa para proximo estágio
+      break;
+    case INIT:
+      // se sinal de pronto tiver sido enviado muda estado para calibrando
+      Serial.print("Alinhe seu automóvel...");
+      if (States.setInitialPositionLeft(distEF, distET))
+      {
+        sidePark = "esquerda";
+        reverseSide = "direita";
+        Serial.print("Posicionamento concluído.");
+        state = States.setState(state);
+      }
+      else if (States.setInitialPositionRight(distDF, distDT))
+      {
+        sidePark = "direita";
+        reverseSide = "esquerda";
+        Serial.print("Posicionamento concluído.");
+        state = States.setState(state);
+      }
+      break;
+    case TURN:
+      Serial.print("Gire o volante todo para ");
+      Serial.print(sidePark);
+      Serial.print(". Em seguida entre na vaga devagar...");
+      if (distT >= 80 && distT <= 90)
+      {
+        Serial.print("Pare o carro!");
+        delay(1000);
+        state = States.setState(state);
+      }
+      break;
+    case ENTRY:
+      Serial.print("Alinhe o volante e continue dando ré devagar...");
+      if (distT >= 60 && distT <= 70)
+      {
+        Serial.print("Pare o carro!");
+        delay(1000);
+        state = States.setState(state);
+      }
+      break;
+    case RETURN:
+      Serial.print("Vire o volante completamente para a ");
+      Serial.print(reverseSide);
+      Serial.print(" agora.");
+
+      break;
+    case ENDUP:
+
+      break;
+    case FINISHED:
+      // Termina a vaga e os sensores são desativados
+      break;
+    default:
+
+      break;
+    }
   }
-
-  Serial.print("Sensor 1: ");
-  Serial.print(dist1);
-
-  Serial.print("\tSensor 2: ");
-  Serial.println(dist2);
+  if (distEF < 45 || distET < 45 || distDF < 45 || distDT < 45)
+  {
+    Serial.print("Atenção, risco de colisão! \n");
+    Serial.print("Medidas do lado esquerdo: \n");
+    Serial.print(distEF, distET);
+    Serial.print("Medidas do lado direito: \n");
+    Serial.print(distDF, distDT);
+  }
 
   delay(100);
 }
